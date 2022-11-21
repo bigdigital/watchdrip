7de9f5b36cf7602600b5b9708981fee5b89107ed
bigdigital <bigdigital.kov@gmail.com>
2022-11-21 08:34:20 +0200

    do not send treatment in json if not defined

--- app/src/main/java/com/thatguysservice/huami_xdrip/models/webservice/WebServiceTreatment.java
+++ app/src/main/java/com/thatguysservice/huami_xdrip/models/webservice/WebServiceTreatment.java
@@ -3,14 +3,17 @@ package com.thatguysservice.huami_xdrip.models.webservice;
 import android.os.Bundle;
 
 public class WebServiceTreatment {
-    public double insulin;
-    public double carbs;
-    public long time;
+    public Double insulin;
+    public Double carbs;
+    public Long time;
 
     public WebServiceTreatment(Bundle bundle) {
         this.insulin = bundle.getDouble("treatment.insulin", -1);
         this.carbs = bundle.getDouble("treatment.carbs", -1);
         this.time = bundle.getLong("treatment.timeStamp", -1);
+        if ( this.insulin == -1)  this.insulin = null;
+        if ( this.carbs == -1)  this.carbs = null;
+        if ( this.time == -1)  this.time = null;
     }
 }
 
