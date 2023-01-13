821397328a8838a2f21aadc52b98dcc580d9dd09
bigdigital <bigdigital.kov@gmail.com>
2023-01-13 09:55:21 +0200

    fix error on load

--- app/src/main/java/com/thatguysservice/huami_xdrip/HuamiXdrip.java
+++ app/src/main/java/com/thatguysservice/huami_xdrip/HuamiXdrip.java
@@ -33,8 +33,8 @@ public class HuamiXdrip extends MultiDexApplication {
     @Override
     public void onCreate() {
         HuamiXdrip.context = getApplicationContext();
-        BroadcastService.initialStartIfEnabled();
         ReactiveAndroidInitialize();
+        BroadcastService.initialStartIfEnabled();
         scheduleTask();
         super.onCreate();
     }
